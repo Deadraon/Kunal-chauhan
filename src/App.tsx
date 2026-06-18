4d3c122ab4cdb4fa8c6a879b9160acf4870429bf
Kunal Chauhan <chauhankunal695@gmail.com>
2026-06-18 22:52:42 +0530

    fix: make background video and radial gradient fixed on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,7 @@ export default function App() {
         loop
         muted={isMuted}
         playsInline
-        className="absolute inset-0 w-full h-full object-cover z-0 pointer-events-none transition-opacity duration-[1500ms]"
+        className="fixed inset-0 w-full h-full object-cover z-0 pointer-events-none transition-opacity duration-[1500ms]"
         src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260314_131748_f2ca2a28-fed7-44c8-b9a9-bd9acdd5ec31.mp4"
       />
 
